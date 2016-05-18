adcdc8c473ab5d042868accfe4b882fcc884a303
Richard Harrington <harrington.richard@gmail.com>
2016-05-18 09:41:54 -0400

    changes

---
+++ lesson07/lesson7-dom-exercise-starter-code/js_dom_exercise/main.js
@@ -0,0 +1,43 @@
+/* DOM Manipulation: Independent Practice
+
+******FIRST******
+First, read this page about something we didn't cover, prompts!
+http://www.w3schools.com/jsref/met_win_prompt.asp
+*****************
+
+To complete this excercise, you must meet the following requirements:
+
+- When the user clicks the "#new-thing-button" button, display a prompt 
+  asking what their favorite things are.
+- When they hit enter, take the value they've typed in and add it to the list 
+  (remember: appendChild)
+
+*/
+
+window.onload = function() {
+  // when someone clicks the button...
+
+  // http://www.w3schools.com/jsref/met_win_prompt.asp
+  // var yourName = prompt("What is your name?");
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+};
+
+/*
+
+Bonus:
+
+When they don't type anything into the prompt, alert the user 
+"you must type in a value!"
+
+Extra Bonus: 
+
+Instead of a prompt, have the user enter new things using an input box.
+When the user presses "submit", the value of the input box should be 
+appended to the list of things, and the input box should be cleared.
+Hint: input elements have a "value" attribute, which is the value that 
+the user is typing in. It can be read and also it can be set.
+
+*/
+
+
